55fc23b4b384c596d847fcf2228d64381435cc8a
Ahmet ÇELİK <ben4hmetcelik@gmail.com>
2024-11-04 15:14:13 +0300

    Send Method Called

---
+++ src/Response.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace GaravelPHP\Response;
+
+class Response
+{
+
+    public $json;
+
+
+    public function response($data, $message = '', $status = 200)
+    {
+        header('Content-Type: application/json');
+        http_response_code($status);
+        $this->json = json_encode([
+            'data' => $data,
+            'status' => $status,
+            'message' => $message
+        ]);
+        return $this->send();
+    }
+
+    public function fail($message = '', $status = 400)
+    {
+        header('Content-Type: application/json');
+        http_response_code($status);
+        $this->json = json_encode([
+            'status' => $status,
+            'message' => $message
+        ]);
+        return $this->send();
+    }
+
+    public function success($data, $message = '', $status = 200)
+    {
+        header('Content-Type: application/json');
+        http_response_code($status);
+        $this->json = json_encode([
+            'data' => $data,
+            'status' => $status,
+            'message' => $message
+        ]);
+        return $this->send();
+    }
+
+    public function error($message = '', $status = 500)
+    {
+        header('Content-Type: application/json');
+        http_response_code($status);
+        $this->json = json_encode([
+            'status' => $status,
+            'message' => $message
+        ]);
+        return $this->send();
+    }
+
+    public function send()
+    {
+        echo $this->json;
+        die();
+    }
+
+    public function notFound($message = 'Not Found', $status = 404)
+    {
+        header('Content-Type: application/json');
+        http_response_code($status);
+        $this->json = json_encode([
+            'status' => $status,
+            'message' => $message
+        ]);
+        return $this->send();
+    }
+
+    public function unauthorized($message = 'Unauthorized', $status = 401)
+    {
+        header('Content-Type: application/json');
+        http_response_code($status);
+        $this->json = json_encode([
+            'status' => $status,
+            'message' => $message
+        ]);
+        return $this->send();
+    }
+
+
+    public function forbidden($message = 'Forbidden', $status = 403)
+    {
+        header('Content-Type: application/json');
+        http_response_code($status);
+        $this->json = json_encode([
+            'status' => $status,
+            'message' => $message
+        ]);
+        return $this->send();
+    }
+
+    public function methodNotAllowed($message = 'Method Not Allowed', $status = 405)
+    {
+        header('Content-Type: application/json');
+        http_response_code($status);
+        $this->json = json_encode([
+            'status' => $status,
+            'message' => $message
+        ]);
+        return $this->send();
+    }
+
+    public function html($content)
+    {
+        header('Content-Type: text/html');
+        echo $content;
+        return;
+    }
+}
